666078d399382a2b3f234c4c641b924dda7403c7
thma <gH1710>
2021-12-03 22:19:10 +0100

    improve the code a bit

--- src/GraphReduction.hs
+++ src/GraphReduction.hs
@@ -10,11 +10,34 @@ import Data.Maybe (fromMaybe)
 
 type Pointer = Int
 
+infixl 5 :@:
 data Graph =
-    Node Pointer Pointer
-  | Comb String
+    Pointer :@: Pointer
+  | Comb Combinator
   | Num  Integer
   deriving (Eq, Show)
+
+data Combinator = I | K | S | B | C | Y | P | ADD | SUB | MUL | DIV | REM | SUB1 | EQL | ZEROP | IF
+                  deriving (Eq, Show)
+
+fromString :: String -> Combinator
+fromString "i" = I
+fromString "k" = K
+fromString "s" = S
+fromString "b" = B
+fromString "c" = C
+fromString "y" = Y
+fromString "p" = P
+fromString "+" = ADD
+fromString "sub" = SUB
+fromString "div" = DIV
+fromString "rem" = REM
+fromString "*" = MUL
+fromString "sub1" = SUB1
+fromString "eq" = EQL
+fromString "is0" = ZEROP
+fromString "if" = IF
+fromString _c = error $ "unknown combinator " ++ _c
 
 type AllocatedGraph = [(Pointer, Graph)]
 
@@ -22,10 +45,9 @@ collectAllReachables :: Pointer -> AllocatedGraph -> AllocatedGraph -> Allocated
 collectAllReachables rootP aGraph result =
   let rootNode = peek rootP aGraph
   in case rootNode of
-    Node l r -> (l, peek l aGraph) : (r, peek r aGraph) : collectAllReachables l aGraph result ++ collectAllReachables r aGraph result ++ result
+    l :@: r -> (l, peek l aGraph) : (r, peek r aGraph) : collectAllReachables l aGraph result ++ collectAllReachables r aGraph result ++ result
     Comb s -> result
     Num n -> result
-
 
 compactify :: Pointer -> AllocatedGraph -> AllocatedGraph
 compactify rootP aGraph = (rootP, peek rootP aGraph) : collectAllReachables rootP aGraph []
@@ -36,7 +58,7 @@ allocate expr =
   alloc expr 1 []
   where
     alloc :: Expr -> Int -> [(Int, Graph)] -> [(Int, Graph)]
-    alloc (Var name) pointer memMap = (pointer, Comb name) : memMap
+    alloc (Var name) pointer memMap = (pointer, Comb $ fromString name) : memMap
     alloc (Int val)  pointer memMap = (pointer, Num val) : memMap
     alloc (l :@ r)   pointer memMap =
       let pointerL = pointer+1
@@ -46,7 +68,7 @@ allocate expr =
           allocR   = alloc r pointerR []
           maxR     = maxPointer allocR
       in
-      (pointer, Node pointerL pointerR) : (allocL ++ allocR ++ memMap)
+      (pointer, pointerL :@: pointerR) : (allocL ++ allocR ++ memMap)
     alloc (Lam _ _)  pointer memMap = error "lambdas should already be abstracted"
 
 maxPointer :: AllocatedGraph -> Pointer
@@ -57,7 +79,7 @@ spine rootP graph stack =
   case peek rootP graph of
     c@(Comb _)   -> (c, stack)
     n@(Num _)    -> (n, stack)
-    g@(Node l r) -> spine l graph ((rootP,g):stack)
+    g@(l :@: r) -> spine l graph ((rootP,g):stack)
 
 
 run :: String -> IO Graph
@@ -91,67 +113,66 @@ step rootP graph =
         _        -> graph
 
 
-apply :: String -> [(Pointer, Graph)] -> AllocatedGraph-> AllocatedGraph
-apply "i" ((p,Node _ xP):_) aGraph =
+apply :: Combinator -> [(Pointer, Graph)] -> AllocatedGraph-> AllocatedGraph
+apply I ((p,_ :@: xP):_) aGraph =
   let xVal = peek xP aGraph
   in  poke p xVal aGraph
-apply "k" ((_p, Node _ xP):(p, Node _ _):_) aGraph =
+apply K ((_p, _ :@: xP):(p, _ :@: _):_) aGraph =
   poke p (peek xP aGraph) aGraph
-apply "s" ((_p1, Node _ xP):(_p2, Node _ yP):(p3, Node _ zP):_) aGraph =
+apply S ((_p1, _ :@: xP):(_p2, _ :@: yP):(p3, _ :@: zP):_) aGraph =
   let maxp = maxPointer aGraph
-      node1 = (maxp+1, Node xP zP)
-      node2 = (maxp+2, Node yP zP)
-  in node1 : node2 : poke p3 (Node (maxp+1) (maxp+2)) aGraph
-apply "b" ((_p1, Node _ xP):(_p2, Node _ yP):(p3, Node _ zP):_) aGraph =
+      node1 = (maxp+1, xP :@: zP)
+      node2 = (maxp+2, yP :@: zP)
+  in node1 : node2 : poke p3 ((maxp+1) :@: (maxp+2)) aGraph
+apply B ((_p1, _ :@: xP):(_p2, _ :@: yP):(p3, _ :@: zP):_) aGraph =
   let maxp = maxPointer aGraph
-      node1 = (maxp+1, Node yP zP)
-  in  node1 : poke p3 (Node xP (maxp+1)) aGraph  
-apply "c" ((_p1, Node _ xP):(_p2, Node _ yP):(p3, Node _ zP):_) aGraph =
+      node1 = (maxp+1, yP :@: zP)
+  in  node1 : poke p3 (xP :@: (maxp+1)) aGraph  
+apply C ((_p1, _ :@: xP):(_p2, _ :@: yP):(p3, _ :@: zP):_) aGraph =
   let maxp = maxPointer aGraph
-      node1 = (maxp+1, Node xP zP)
-  in  node1 : poke p3 (Node (maxp+1) yP) aGraph
-apply "y" ((p1, Node yP fP) : _) aGraph =
+      node1 = (maxp+1, xP :@: zP)
+  in  node1 : poke p3 ((maxp+1) :@: yP) aGraph
+apply Y ((p1, yP :@: fP) : _) aGraph =
   -- copying version of Y-combinator works nicely
   let maxp = maxPointer aGraph
-      node1 = (maxp+1, Node yP fP)
-  in  node1 : poke p1 (Node fP (maxp+1)) aGraph
+      node1 = (maxp+1, yP :@: fP)
+  in  node1 : poke p1 (fP :@: (maxp+1)) aGraph
   -- the not-copying version does not yet work, sigh...
-  --poke p1 (Node fP p1) aGraph
+  --poke p1 (fP :@: p1) aGraph
 {--
 |apply (Y,(node as ref(app((_,f),m,q)))::_) =
         node := app((f,node),ref Eval,q)
 --}
 
-apply "if" ((_p1, Node _ testP):(_p2, Node _ xP):(p3, Node _ yP):_) aGraph =
+apply IF ((_p1, _ :@: testP):(_p2, _ :@: xP):(p3, _ :@: yP):_) aGraph =
   case subEval testP aGraph of
     Num test -> if test == 1
                   then poke p3 (peek xP aGraph) aGraph
                   else poke p3 (peek yP aGraph) aGraph
-    Comb k   -> error $ "does not evaluate to bool: 0" ++ k
-    n@(Node l r) -> error $ "should not happen" ++ show n  
-
-apply "+" ((_p1, Node _ xP) : (p2, Node _ yP):_) aGraph = 
+    Comb k   -> error $ "does not evaluate to bool: 0" ++ show k
+    n@(l :@: r) -> error $ "should not happen" ++ show n  
+apply ADD ((_p1, _ :@: xP) : (p2, _ :@: yP):_) aGraph = 
   let (Num x) = subEval xP aGraph
       (Num y) = subEval yP aGraph
       result  = x + y
   in poke p2 (Num result) aGraph
-apply "*" ((_p1, Node _ xP) : (p2, Node _ yP):_) aGraph = 
+apply MUL ((_p1, _ :@: xP) : (p2, _ :@: yP):_) aGraph = 
   let (Num x) = subEval xP aGraph
       (Num y) = subEval yP aGraph
       result  = x * y
   in poke p2 (Num result) aGraph
-apply "sub" ((_p1, Node _ xP) : (p2, Node _ yP):_) aGraph = 
+apply SUB ((_p1, _ :@: xP) : (p2, _ :@: yP):_) aGraph = 
   let (Num x) = subEval xP aGraph
       (Num y) = subEval yP aGraph
       result  = x - y
   in poke p2 (Num result) aGraph
-apply "/" ((_p1, Node _ xP) : (p2, Node _ yP):_) aGraph = 
+apply DIV ((_p1, _ :@: xP) : (p2, _ :@: yP):_) aGraph = 
   let (Num x) = subEval xP aGraph
       (Num y) = subEval yP aGraph
       result  = x `div` y
   in poke p2 (Num result) aGraph
 
-apply "eq" ((_p1, Node _ xP) : (p2, Node _ yP):_) aGraph = 
+apply EQL ((_p1, _ :@: xP) : (p2, _ :@: yP):_) aGraph = 
   let x = subEval xP aGraph
       y = subEval yP aGraph
       result  = if x == y then 1 else 0
